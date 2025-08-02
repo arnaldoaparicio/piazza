101e936fec75143f54c1103ca94de05181ba0033
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-02 18:11:38 -0400

    Fix validation for 'email'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :email,
+  validates :email,
     format: { with: URI::MailTo::EMAIL_REGEXP },
     uniqueness: { case_sensitive: false }
 end
