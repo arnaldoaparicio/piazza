f8dcd14beec96a8efef7906b6c158d79b0d741d9
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-02 18:11:55 -0400

    Add validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+  end
   context 'creating a new User' do
     it 'requires a name' do
       user = User.new(name: '', email: 'johndoe@example.com')
