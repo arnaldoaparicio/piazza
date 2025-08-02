d77db0fde863941589087bf3bc1212734080b2cb
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-02 17:45:06 -0400

    Add formatting for 'email' attribute with built-in E-Mail regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :email, uniqueness: true
-  
+  validates_presence_of :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    uniqueness: { case_sensitive: false }
 end
