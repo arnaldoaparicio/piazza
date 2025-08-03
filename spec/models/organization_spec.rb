9d1b82597a91d220c6c4dec6b8d43eff775c4413
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-03 17:17:03 -0400

    Add tests for Organization relationships

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,1 +1,8 @@
 require 'rails_helper'
+
+RSpec.describe Organization, type: :model do
+  describe 'relationships' do
+    it { should have_many(:memberships) }
+    it { should have_many(:members).through(:memberships).source(:user) }
+  end
+end
