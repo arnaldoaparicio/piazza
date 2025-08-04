1681aaf479be425d2376128fe8bfb430b0077b8c
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-04 18:07:20 -0400

    Adjust formatting

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   it 'formats page specific title' do
-    content_for(:title) { 'Page Title'}
+    content_for(:title) { 'Page Title' }
 
     expect("Page Title | #{I18n.t('piazza')}").to eq(title)
   end
