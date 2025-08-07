dc658bfd3a38e40301f50f4d5ee129217c952159
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-07 13:54:18 -0400

    Add test to check for successful completion of form for User

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "users/new.html.erb", type: :feature do
+  it 'fills out and successfully submits form and redirect to root' do
+    visit '/sign_up'
+
+    expect(page).to have_content('Sign up for Piazza')
+    expect(page).to have_button('Sign up!')
+
+    fill_in 'Name', with: 'Johnny'
+    fill_in 'Email', with: 'johnstamos@example.com'
+    fill_in 'Password', with: '123abcdef'
+
+    click_button 'Sign up!'
+
+    expect(page).to have_current_path('/')
+  end
+end
