7a106ce8e6cd42fa88f9144d4d08384b9d3ba73e
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-12 20:00:40 -0400

    Adjust formatting

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -1,7 +1,7 @@
 ActionView::Base.field_error_proc = -> (html_tag, instance) {
   unless html_tag =~ /^<label/
     html = Nokogiri::HTML::DocumentFragment.parse(html_tag)
-    html.children.add_class("is-danger")
+    html.children.add_class('is-danger')
 
     error_message_markup = <<~HTML
       <p class='help is-danger'>
