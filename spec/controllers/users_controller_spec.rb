f24c1ccf97d0bcbdae982241dee34e6faab51b3e
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-15 18:19:02 -0400

    Add test to check for rendering of errors if input is invalid

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,7 +15,21 @@ RSpec.describe UsersController, type: :controller do
     end.to change { User.count }.from(0).to(1).and change { Organization.count }.from(0).to(1)
 
     expect(response).to redirect_to(root_path)
-    # require 'pry'; binding.pry
-    # expect(response).to have_tag(".notification.is-success", I18n.t("users.create.welcome", name: "John"))
+  end
+
+  it 'renders errors if input is invalid' do
+    get :new
+
+    expect do
+      post :create, params: { user: {
+        name: 'John',
+        email: 'johndoe@example.com',
+        password: 'pass'
+      }
+    }
+    end.to_not change { User.count }
+    expect(Organization.all.count).to eq(0)
+    expect(response).to have_http_status(:unprocessable_content)
+    expect(response).to_not redirect_to(root_path)
   end
 end
