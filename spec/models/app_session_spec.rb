4c0f5b8875fcc957e586918004faeb6d7b1b2de9
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-21 21:42:26 -0400

    Relocated file into spec folder

--- spec/models/app_session_spec.rb
+++ spec/models/app_session_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AppSession, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+  before(:each) do
+    @user = users(:jerry)
+  end
+
+  it 'generates and saves a token when a new record is created' do
+    app_session = @user.app_sessions.create
+
+    expect(app_session.persisted?).to eq(true)
+    expect(app_session.token_digest).to_not eq(nil)
+    binding.pry
+  end
 end
