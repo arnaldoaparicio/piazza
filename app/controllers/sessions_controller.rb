1dd3695c7a52d399cffbe653a705ccbe392efaa3
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-08-27 14:16:16 -0400

    Implement session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @app_session = User.create_app_session(email: login_params[:email], password: login_params[:password])
+
+    if @app_sessions
+      # TODO: Store details in cookie
+      
+      flash[:success] = t('.success')
+      redirect_to root_path, status: :see_other
+    else
+    end
   end
 end
