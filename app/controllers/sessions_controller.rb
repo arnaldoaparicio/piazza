58b0fad79e81ebb0b58e76ce1d526215335308a4
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-09-15 18:54:37 -0400

    Add 'log_in' method to 'create' action to store user data in cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @app_session = User.create_app_session(email: login_params[:email], password: login_params[:password])
 
     if @app_session
-      # TODO: Store details in cookie
+      log_in(@app_session)
 
       flash[:success] = t('.success')
       redirect_to root_path, status: :see_other
