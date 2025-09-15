18bbf8c1e2d95edfac64bee9376bedc0ffdcc670
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-09-15 19:22:27 -0400

    Rename file

--- app/controllers/concerns/autheticate.rb
+++
@@ -1,9 +0,0 @@
-module Authenticate
-  extend ActiveSupport::Concern
-
-  protected
-
-  def log_in(app_session)
-    cookies.encrypted.permanent[:app_session] = { value: app_session.to_h }
-  end
-end
