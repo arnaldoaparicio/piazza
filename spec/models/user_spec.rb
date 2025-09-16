afc4e7de0f72dbf6ee5d29d0f654d80484d4487e
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-09-16 15:51:48 -0400

    Add 'user' fixtures

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  fixtures :users
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:email).case_insensitive }
